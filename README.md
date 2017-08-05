aa64353e9015c048a23359806a554ff75499b519
Jon Jagger <jon@jaggersoft.com>
2017-08-05 08:47:33 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+
+## chennai-students-serverless
+A simple html,js,css app for remembering people's faces.
+
+Based on https://github.com/ggoral/sinatra-modular-example
+A version served by sinatra is at https://github.com/JonJagger/chennai-students-server
---
+++ js/memory.js
@@ -0,0 +1,51 @@
+$(document).ready(function() {
+
+  let ipAddress = function() {
+    return 'http://104.197.157.118';
+  };
+
+  let image = function(filename) {
+    return $('<img>', {
+      'class':'student',
+      'title':filename.substring(0,filename.indexOf('.')),
+      'src':`${ipAddress()}/img/${filename}`
+     });
+  };
+
+  let tipped = function(node) {
+    return node.tooltip({
+      position: {
+        my: 'center bottom-20',
+        at: 'center top',
+        using: function(position,feedback) {
+          $(this).css(position);
+          $('<div>')
+            .addClass('arrow')
+            .addClass(feedback.vertical)
+            .addClass(feedback.horizontal)
+            .appendTo(this);
+        }
+      }
+    });
+  };
+
+  $('#shuffle').click(function() {
+    window.location.reload();
+  });
+
+  $.ajax({
+    url: `${ipAddress()}/images/index?shuffled=true`,
+    crossDomain:true,
+    success: function(html){
+      $(html).find('li').each(function() {
+        let filename = $(this).text();
+        let student = image(filename);
+        $('students').append(tipped(student));
+      });
+    },
+    error:function(xhr, textStatus, errorThrown) {
+      alert('textStatus:'+textStatus);
+    }
+  });
+
+});
